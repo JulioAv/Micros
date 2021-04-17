8daebe3e67393013cd5c1c7fc5b5b6d0061df11c
JulioAv <avi19285@uvg.edu.gt>
2021-04-16 21:20:55 -0600

    16

--- Lab7.X/Lab7.c
+++ Lab7.X/Lab7.c
@@ -32,24 +32,25 @@ char uni= 0;
 int disp;
 char display[10] = {0B00111111, 0B00000110, 0B01011011, 0B01001111, 
 0B01100110, 0B01101101, 0B01111101, 0B01000111, 0B01111111, 0B01100111};
+//Valores para el display
 
 void __interrupt()isr(void){
-    if (RBIF){
-        if (RB0==0){
-            PORTA++;
-            INTCONbits.RBIF = 0;
+    if (RBIF){                      //Verifica si hay interrupción y
+        if (RB0==0){                //dependiendo del pin presionado
+            PORTA++;                //se incrementa o decrementa el valor
+            INTCONbits.RBIF = 0;    //del Puerto A y se limpia el IF
     }
         else if (RB1==0) {
             PORTA--;
             INTCONbits.RBIF = 0;
     }
 }
-    if (T0IF) {
-        switch (disp){
-            case 0: 
-                PORTD = 0x00;
-                PORTC = 0x00;
-                PORTD = 0x04;
+    if (T0IF) {                     //Verifica si hay overflow del tmr0
+        switch (disp){              //y dependiendo del display que
+            case 0:                 //toque desplegar, llama al respectivo
+                PORTD = 0x00;       //valor desde el arreglo display
+                PORTC = 0x00;       //limpia los puertos C y D y designa
+                PORTD = 0x04;       //cual será el siguiente display
                 PORTC = display[uni];
                 disp = 1;
                 break;
@@ -68,25 +69,25 @@ void __interrupt()isr(void){
                 disp =0;
                 break;
         }
-        PORTBbits.RB2=~PORTBbits.RB2;
-        INTCONbits.T0IF = 0;
+        //PORTBbits.RB2=~PORTBbits.RB2;
+        INTCONbits.T0IF = 0;    //Se limpia el IF y se reinicia
         TMR0 = 217;
         
     }
 }
 
 void main(void) {
-    ANSEL = 0x00;
+    ANSEL = 0x00;           // Solo puertos digitales
     ANSELH = 0x00;
     
     TRISA = 0x00;
-    TRISB = 0x03;
-    TRISC = 0x00;
+    TRISB = 0x03;           // Los primeros dos pines como inputs
+    TRISC = 0x00;           // Todo lo demás son salidas
     TRISD = 0x00;
     
     OSCCON = 0B0110111;      // Oscilador a 500KHz
     OPTION_REG = 0B01000011; // TMR0 con 16 de prescaler
-    TMR0 = 217;
+    TMR0 = 217;             // Overflow a los 5ms
     IOCB = 0x03;
     
     PORTA = 0x00;
@@ -94,15 +95,15 @@ void main(void) {
     PORTD = 0X00;
     
     INTCONbits.GIE = 1;
-    INTCONbits.T0IE = 1;
+    INTCONbits.T0IE = 1;    //Activación de las interrupciones
     INTCONbits.RBIE = 1;
         
     while (1){
-        cont = PORTA;
-        cen = PORTA/100;
-        val1 = cont%100;
-        dec = val1/10;
-        uni = val1%10;
+        cont = PORTA;       
+        cen = PORTA/100;    // Las centenas son la división sin decimales
+        val1 = cont%100;    
+        dec = val1/10;      //Las decenas son la división del residuo
+        uni = val1%10;      //Las unidades son el residuo de la division
     }
     
 }
